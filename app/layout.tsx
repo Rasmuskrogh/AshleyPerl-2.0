1922a1a7cd36272b99dfeef60da3eafc04ce2089
[Rasmus Krogh-Andersen] <rasmus.kroghandersen@gmail.com>
2025-10-02 17:17:45 +0200

    making the about picture more dynamic and adding the correct favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,6 +15,21 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta name="color-scheme" content="light only" />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/AP-mini-logo.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/AP-mini-logo.png"
+        />
+        <link rel="apple-touch-icon" href="/AP-mini-logo.png" />
+        <link rel="shortcut icon" href="/AP-mini-logo.png" />
+        <meta name="msapplication-TileImage" content="/AP-mini-logo.png" />
         <PersonStructuredData />
         <OrganizationStructuredData />
       </head>
